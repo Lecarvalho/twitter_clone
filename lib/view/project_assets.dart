e7c67b3c216c9ca6413250f44c32f9866b4cb045
Leandro Carvalho <cadastro@lecarvalho.net>
2021-02-13 17:04:57 -0500

    created project assets class

---
+++ lib/view/project_assets.dart
@@ -0,0 +1,31 @@
+class AssetsBase {
+}
+
+class LogoAssets {
+  static const String _assetsBasePath = "assets/logos/";
+
+  static final twitter = _assetsBasePath + "twitter.png";
+  static final flutterTwitterClone = _assetsBasePath + "flutter-twitter-clone.png";
+}
+
+class IconAssets {
+
+  static const String _assetsBasePath = "assets/icons/";
+
+  static final addTweet = _assetsBasePath + "add-tweet.png";
+  static const appIcon = _assetsBasePath + "app-icon.png";
+  static const comment = _assetsBasePath + "comment.png";
+  static const feature = _assetsBasePath + "feature.png";
+  static const heartSolidDarken = _assetsBasePath + "heart-solid-darken.png";
+  static const heartSolid = _assetsBasePath + "heart-solid.png";
+  static const heart = _assetsBasePath + "heart.png";
+  static const homeSolid = _assetsBasePath + "home-solid.png";
+  static const home = _assetsBasePath + "home.png";
+  static const notificationSolid = _assetsBasePath + "notification-solid.png";
+  static const notification = _assetsBasePath + "notification.png";
+  static const profileSolid = _assetsBasePath + "profile-solid.png";
+  static const profile = _assetsBasePath + "profile.png";
+  static const retweet = _assetsBasePath + "retweet.png";
+  static const searchSolid = _assetsBasePath + "search-solid.png";
+  static const search = _assetsBasePath + "search.png";
+}
